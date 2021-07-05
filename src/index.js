dc25da4325a9f8afa225df3d565cda61a192fb1d
kennymiesner <kennymiesner@gmail.com>
2021-07-05 13:15:01 -0500

    Completed Guided Project

--- src/index.js
+++ src/index.js
@@ -36,29 +36,50 @@ console.log(aboutNavItem)
 
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
+logoTitle.textContent = 'Lambda Dog'
+titleFirstCard.textContent = 'I rulez'
+subtitleFirstCard.textContent = 'obviously'
+textFirstCard.textContent = 'Doggo ipsum the neighborhood pupper much ruin diet sub woofer heckin good boys and girls, length boy long woofer.'
+link2FirstCard.textContent = 'Dog Ipsum'
 //  B- Have the students research online the difference between textContent and innerText
 
 
 // 👉 4- Changing any property
 //  A- Using dot notation to change a few attributes
+logoTitle.className = 'heading logo banana'
+imageFirstCard.src = 'https://herepup.com/wp-content/uploads/2015/12/Dog-Advice-1-1.jpg'
 //  B- Using .setAttribute to change a few attributes
+link1FirstCard.setAttribute('href', 'https://www.pexels.com/search/dog')
+link2FirstCard.setAttribute('href', 'https://www.doggoipsum.com')
 
 
 // 👉 5- Changing the styling of an element
 //  A- By changing the class names on the element with the classList API
+header.classList.add('sky')
+header.classList.remove('sky')
+setInterval(() => {
+    header.classList.toggle('sky')
+}, 1000)
 //  B- By manipulating inline styles on the element
+header.style.fontSize = '2em'
 
 
 // 👉 6- Creating new elements from scratch and appending them
 // Create a new link inside the nav for "Blog"
+const blogLink = document.createElement('a')
+blogLink.textContent = 'Blog'
+blogLink.href = '#'
+document.querySelector('nav').appendChild(blogLink)
 
 
 // 👉 7- Making a copy of the card and appending it to the card group
 // DOM nodes can only exist in one spot in the DOM
 // We cannot append the same copy multiple times
+const secondCard = firstCard.cloneNode(true)
+document.querySelector('.card-group').appendChild(secondCard)
 
 
 // 👉 8- Removing an existing element and putting it back [STRETCH if time allows]
-
+.secondCard.prepend()
 
 // 👉 9- Show to students the insertAdjacentHTML method [STRETCH if time allows]
